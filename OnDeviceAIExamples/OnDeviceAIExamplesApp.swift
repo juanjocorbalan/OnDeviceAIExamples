2d999c391d0c03bab2978045633684a14ee3a30f
Juanjo Corbalán <juanjocorbalan@gmail.com>
2025-06-10 16:53:57 +0200

    Initial Commit

---
+++ OnDeviceAIExamples/OnDeviceAIExamplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnDeviceAIExamplesApp.swift
+//  OnDeviceAIExamples
+//
+//  Created by Juanjo Corbalan on 10/6/25.
+//
+
+import SwiftUI
+
+@main
+struct OnDeviceAIExamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
